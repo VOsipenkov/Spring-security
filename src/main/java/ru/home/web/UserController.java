e04131aa01a08d51e8fed2d176c3454702ad5c87
VOsipenkov <VOsipenkov@lanit.ru>
2022-07-23 11:26:36 +0300

    Add user api

---
+++ src/main/java/ru/home/web/UserController.java
@@ -0,0 +1,22 @@
+package ru.home.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "Users";
+    }
+
+    @GetMapping("/admins")
+    public String getAdmins() {
+        return "Admins";
+    }
+}
